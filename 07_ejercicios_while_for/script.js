2264c9d00277d16b508b2cdf1112f9372ecb1ed0
AraelA28103 <yuliethgonzalez@liceovvh.cl>
2025-10-20 09:24:59 -0300

    .

---
+++ 07_ejercicios_while_for/script.js
@@ -0,0 +1,20 @@
+// ejemplos while - for
+// Contar números del 10 al 1
+function ejecutarWhile() {
+    let resultado = "El resultado es: ";
+    let count = 10;
+    while (count > 0) {
+        resultado += ' $(count)';
+
+        count--
+    }
+    alert(resultado);
+}
+
+/* Ejercicio 1 
+Bucle while: Imprimir del 1 a n (n es ingresado por teclado).
+Escribe un programa que utilice un bucle while para imprimir los números del 1 al n con un solo Alert.
+*/
+function imprimirNum() {
+    let 
+}
