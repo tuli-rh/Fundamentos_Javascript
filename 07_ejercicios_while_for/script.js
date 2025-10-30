382d6edf8c698566d7244963b72b0bc94e235d06
AraelA28103 <yuliethgonzalez@liceovvh.cl>
2025-10-30 15:07:56 -0300

    .

--- 07_ejercicios_while_for/script.js
+++ 07_ejercicios_while_for/script.js
@@ -77,9 +77,82 @@ Pide un número al usuario e imprime su tabla de multiplicar del 1 al 10 en un s
 function tablaMultiplicar() {
     let num = parseInt(prompt("Ingre un número: "));
     let tablas = `Tabla de ${num}: `;
-    
-    for(let i = 1; i <= 10; i++) {
-        tablas += `\n ${num} x ${1} = `
+
+    for (let i = 1; i <= 10; i++) {
+        tablas += `\n ${num} x ${1} = ${num + 1}`
     }
+    alert(tablas);
 }
 
+/*Bucle for: Números pares hasta n
+Solicita un número n e imprime todos los números pares desde 1 hasta n usando un bucle for.
+
+Ejemplo de resultado:
+2, 4, 6, 8, 10
+*/
+
+function numerosPares() {
+    let num = parseInt(prompt("Ingre un número: "));
+    let resultado = `Los números pares de ${num} son: `;
+
+    for (let i = 1; i <= num; i++) {
+        if (i % 2 == 0) {
+            resultado += `${i} `
+        }
+    }
+    alert(resultado);
+}
+
+/*Bucle for: Contar letras de una palabra
+Pide una palabra al usuario y muestra cuántas letras tiene usando un bucle for para recorrerla.
+
+Ejemplo de resultado:
+La palabra "Programación" tiene 12 letras
+*/
+
+function contarPalabra() {
+    let word = parseInt(prompt("Ingre una palabra: "));
+    let count = 0;
+    for (let i = 0; i <= word.lenght; i++) {
+        count++
+    }
+    alert(`La palabra ${word} tiene ${count} letras`);
+}
+
+/*Bucle for: Calcular factorial
+Solicita un número n e imprime el factorial de ese número (n × n-1 × n-2 × … × 1) usando un bucle for.
+
+Ejemplo de resultado:
+El factorial de 5 es 120.
+*/
+
+function sumaNumerosImpares() {
+    let num = parseInt(prompt("Ingre un número para calcular su factorial: "));
+    let factorial = 1;
+    let largo = "1"
+
+    for (let i = 2; i <= num; i++) {
+        largo += ` * ${i}`
+        factorial *= i;
+    }
+    alert(`El factorial de ${num} es : ${largo} = ${factorial}`);
+}
+
+/* Bucle for: Suma de números impares hasta n
+Pide un número n y suma todos los números impares desde 1 hasta n.
+Muestra el resultado con alert.
+
+Ejemplo de resultado:
+La suma de impares hasta 9 es 25.
+*/
+
+function sumaNumerosImpares() {
+    let num = parseInt(prompt("Ingre un número: "));
+    let resultado = 0;
+    let largoNumero = "1"
+
+    for (let i = 2; i <= num; i++) {
+        largo += 
+    }
+    alert(resultado);
+}
